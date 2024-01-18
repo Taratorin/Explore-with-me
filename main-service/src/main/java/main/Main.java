f23a97a55dd813c7899b11e0bf8cde1b9c745b8e
Taratorin <unknownmanty@yandex.ru>
2024-01-18 19:28:56 +0300

    add StatsClient

--- main-service/src/main/java/main/Main.java
+++ main-service/src/main/java/main/Main.java
@@ -1,10 +1,14 @@
 //package main;
 //
 //import org.springframework.http.ResponseEntity;
+//import org.springframework.http.server.ServletServerHttpRequest;
 //import org.springframework.web.client.RestTemplate;
 //import ru.practicum.ewm.client.stats.StatsClient;
 //import ru.practicum.ewm.dto.stats.EndpointHitDto;
+//import ru.practicum.ewm.dto.stats.HttpRequestDto;
 //
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletRequestWrapper;
 //import java.util.Map;
 //
 //public class Main {
@@ -13,13 +17,10 @@
 //    // todo удалить после окончания работы над main-service
 //    public static void main(String[] args) {
 //        StatsClient client = new StatsClient(new RestTemplate());
-//        EndpointHitDto endpointHitDto = EndpointHitDto.builder()
-//                .app("ewm-main-service")
-//                .uri("/events")
-//                .ip("121.0.1.1")
-//                .timestamp("2024-01-17 05:52:30")
-//                .build();
-//        client.post("http://localhost:9090/hit", endpointHitDto);
+//        HttpRequestDto httpRequestDto = new HttpRequestDto();
+//        HttpServletRequest request = new HttpServletRequestWrapper(new Http);
+//
+//        client.saveHit("http://localhost:9090/hit", );
 //
 //        String start = "2020-05-05 00:00:00";
 //        String end = "2035-05-05 00:00:00";
--- stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/StatsClient.java
@@ -1,11 +1,14 @@
 package ru.practicum.ewm.client.stats;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.ewm.dto.stats.EndpointHitDto;
 import ru.practicum.ewm.dto.stats.HttpRequestDto;
-import ru.practicum.ewm.dto.stats.mapper.HttpRequestDtoMapper;
+import ru.practicum.ewm.stats.mapper.HttpRequestDtoMapper;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -15,28 +18,22 @@ public class StatsClient extends BaseClient {
     private static final String API_PREFIX_POST = "/hit";
     private static final String API_PREFIX_GET = "/stats";
 
-    public StatsClient(RestTemplate rest) {
-        super(rest);
+    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
     }
 
     public void saveHit(HttpRequestDto httpRequestDto) {
         EndpointHitDto endpointHitDto = HttpRequestDtoMapper.toEndpointHitDto(httpRequestDto);
-        super.post("${stats-server.url}" + API_PREFIX_POST, endpointHitDto);
+        post(API_PREFIX_POST, endpointHitDto);
     }
 
     public ResponseEntity<Object> get(HttpServletRequest request) {
         Map<String, String[]> parameters = request.getParameterMap();
-        return super.get("${stats-server.url}" + API_PREFIX_GET, parameters);
-    }
-
-    private static ResponseEntity<Object> prepareResponse(ResponseEntity<Object> response) {
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response;
-        }
-        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
-        if (response.hasBody()) {
-            return responseBuilder.body(response.getBody());
-        }
-        return responseBuilder.build();
+        return get(API_PREFIX_GET, parameters);
     }
 }
