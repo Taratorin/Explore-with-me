65b18c67ee1d9aced9822b29f6958730b4869227
Taratorin <unknownmanty@yandex.ru>
2024-01-27 16:44:50 +0300

    fix docker compose

--- main-service/src/main/java/ru/practicum/config/StatsClientConfig.java
+++ main-service/src/main/java/ru/practicum/config/StatsClientConfig.java
@@ -1,21 +1,21 @@
-package ru.practicum.config;
-
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.ewm.client.stats.StatsClient;
-
-@Configuration
-public class StatsClientConfig {
-
-    @Bean
-    public StatsClient statsClient() {
-        RestTemplateBuilder builder = new RestTemplateBuilder();
-        builder.uriTemplateHandler(new DefaultUriBuilderFactory("${stats-server.url}"))
-                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                .build();
-        return new StatsClient("${stats-server.url}", builder);
-    }
-}
+//package ru.practicum.config;
+//
+//import org.springframework.boot.web.client.RestTemplateBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+//import org.springframework.web.util.DefaultUriBuilderFactory;
+//import ru.practicum.ewm.client.stats.StatsClient;
+//
+//@Configuration
+//public class StatsClientConfig {
+//
+//    @Bean
+//    public StatsClient statsClient() {
+//        RestTemplateBuilder builder = new RestTemplateBuilder();
+//        builder.uriTemplateHandler(new DefaultUriBuilderFactory("${stats-server.url}"))
+//                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+//                .build();
+//        return new StatsClient("${stats-server.url}", builder);
+//    }
+//}
